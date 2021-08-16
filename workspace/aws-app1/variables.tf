1bb7d4c58e5dbc06e6085210136d00932b1d77b7
Paul Kelly <paultkelly@gmail.com>
2021-08-15 19:15:11 -0700

    access key

--- workspace/aws-app1/variables.tf
+++ workspace/aws-app1/variables.tf
@@ -1,9 +1,9 @@
-variable "access_key" {
+variable "AWS_ACCESS_KEY_ID" {
   type = string
   description = "AWS Access Key ID used to connect to provider."
 }
 
-variable "secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
   type = string
   description = "AWS Secret Access Key used to connect to provider."
 }
