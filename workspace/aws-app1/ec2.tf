3e7dd61418d43ea3d7cd5de736f0ac400be7890f
Paul Kelly <paultkelly@gmail.com>
2021-08-15 19:52:19 -0700

    zero out

--- workspace/aws-app1/ec2.tf
+++ workspace/aws-app1/ec2.tf
@@ -1,20 +1,19 @@
-module "ec2_cluster" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "~> 2.0"
+# module "ec2_cluster" {
+#   source                 = "terraform-aws-modules/ec2-instance/aws"
+#   version                = "~> 2.0"
 
-  name                   = "my-cluster"
-  instance_count         = 2
+#   name                   = "my-cluster"
+#   instance_count         = 2
 
-  ami                    = "ami-0443305dabd4be2bc"
-  ebs_optimized          = true
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+#   ami                    = "ami-0443305dabd4be2bc"
+#   instance_type          = "t2.micro"
+#   key_name               = "user1"
+#   monitoring             = true
+#   vpc_security_group_ids = ["sg-12345678"]
+#   subnet_id              = "subnet-eddcdzz4"
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
