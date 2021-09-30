6f1e4ceed6a427756cf45389ed643c96826986b2
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-09-29 20:34:13 -0700

    Fix vpc name

--- workspace/aws-asif/security-groups.tf
+++ workspace/aws-asif/security-groups.tf
@@ -3,7 +3,7 @@ module "ec2_sg" {
 
   name        = "ec2-sg"
   description = "Security group for ec2 instances"
-  vpc_id      = aws_vpc.example.id
+  vpc_id      = aws_vpc.main.id
 
   ingress_cidr_blocks = ["104.32.35.165/32"]
   ingress_rules       = ["ssh-tcp"]
