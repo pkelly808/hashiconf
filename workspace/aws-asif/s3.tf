90c14e06b7c194936b81cfa77db2a553697341aa
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-09-29 20:37:31 -0700

    Update bucket name

--- workspace/aws-asif/s3.tf
+++ workspace/aws-asif/s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "asif-s3-bucket-${var.environment}"
   acl    = "private"
 
   versioning = {
